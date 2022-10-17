04f5ae891c35d82336665c8be7b32f0c69e1578c
YF91925352 <zyf91925352@outlook.com>
2022-10-17 23:09:48 +0200

    uodate Housey-pricey

--- javascript/javascript1/week1/Housey-pricey.js
+++ javascript/javascript1/week1/Housey-pricey.js
@@ -1,6 +1,31 @@
-let width = +prompt("Please enter the width of your house");
-let deepth = +prompt("Please enter the deepth of your house");
-let height = +prompt("Please enter the height of your house");
-let gardenSizeInM2 = +prompt("Please enter the garden size of your house");
-let housePrice = width * deepth * height * 2.5 * 1000 + gardenSizeInM2 * 300;
-console.log(housePrice);
+let peterWidth = +prompt("Please enter the width of Peter house");
+let peterDeepth = +prompt("Please enter the deepth of Peter house");
+let peterHeight = +prompt("Please enter the height of Peter house");
+let peterGardenSizeInM2 = +prompt(
+  "Please enter the garden size of Peter house"
+);
+let peterHousePrice =
+  peterWidth * peterDeepth * peterHeight * 2.5 * 1000 +
+  peterGardenSizeInM2 * 300;
+let peterPayPrice = +prompt("Please enter the price Peter should pay");
+if (peterHousePrice > peterPayPrice) {
+  console.log("You should buy it.");
+} else {
+  console.log("You should consider about the price.");
+}
+
+let juliaWidth = +prompt("Please enter the width of julia house");
+let juliaDeepth = +prompt("Please enter the deepth of julia house");
+let juliaHeight = +prompt("Please enter the height of julia house");
+let juliaGardenSizeInM2 = +prompt(
+  "Please enter the garden size of julia house"
+);
+let juliaHousePrice =
+  juliaWidth * juliaDeepth * juliaHeight * 2.5 * 1000 +
+  juliaGardenSizeInM2 * 300;
+let juliaPayPrice = +prompt("Please enter the price julia should pay");
+if (juliaHousePrice > juliaPayPrice) {
+  console.log("You should buy it.");
+} else {
+  console.log("You should consider about the price.");
+}
