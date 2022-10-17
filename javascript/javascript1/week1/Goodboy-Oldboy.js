27b3312ab7cb34b741c1a12d9e5578db9992b4fe
YF91925352 <zyf91925352@outlook.com>
2022-10-17 17:38:08 +0200

    add Goodboy-Oldboy

---
+++ javascript/javascript1/week1/Goodboy-Oldboy.js
@@ -0,0 +1,9 @@
+let dogyearOfBirth = prompt("Please enetr your dog birth year");
+let dogyearOfBirthNumber = Number(dogyearOfBirth);
+let dogyearFuture = prompt("Please enter the future year");
+let dogyearFutureNumber = Number(dogyearFuture);
+let humanYearAge = dogyearFutureNumber - dogyearOfBirthNumber;
+let dogYearAge = 10 * humanYearAge;
+let shouldShowResultInDogYears = prompt("Do you want to calculate in dog years?")
+if (shouldShowResultInDogYears == "yes") { console.log("Your dog will be " + dogYearAge + " dog years old in " + dogyearFuture + "."); }
+else { console.log("Your dog will be " + humanYearAge + " human years old in " + dogyearFuture + "."); };
