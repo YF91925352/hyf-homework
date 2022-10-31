c39d92821de6c79d5e8bc9c862bc9905d5d0f53f
YF91925352 <zyf91925352@outlook.com>
2022-10-31 13:54:21 +0100

    update studentManager.js

--- javascript/javascript1/week2/studentManager.js
+++ javascript/javascript1/week2/studentManager.js
@@ -1,31 +1,29 @@
 const class07Students = [];
 function addStudentToClass(studentName) {
   if (studentName === "") {
-    alert("You cannot add an empty name to a class");
+    console.log("You cannot add an empty name to a class");
   } else {
     if (class07Students.indexOf(studentName) !== -1) {
-      alert(`Student ${studentName} is already in the class`);
+      console.log(`Student ${studentName} is already in the class`);
     } else {
       if (class07Students.length >= 6 && studentName !== "Queen") {
-        alert("Cannot add more students to class 07");
+        console.log("Cannot add more students to class 07");
       } else {
         class07Students.push(studentName);
       }
     }
   }
 }
-function getNumberOfStudents(studentName) {
+function getNumberOfStudents() {
   console.log(`Now you have ${class07Students.length} students in Class07.`);
 }
 addStudentToClass("tim");
 addStudentToClass("");
 addStudentToClass("andy");
-getNumberOfStudents("andy");
 addStudentToClass("amy");
 addStudentToClass("amy");
 addStudentToClass("pete");
 addStudentToClass("Sam");
 addStudentToClass("tom");
 addStudentToClass("Queen");
-getNumberOfStudents("Queen");
-console.log(class07Students);
+getNumberOfStudents();
