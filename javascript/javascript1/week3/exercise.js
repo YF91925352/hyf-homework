7583f2474988b9e3597d437931029a1e83f7191b
YF91925352 <zyf91925352@outlook.com>
2022-11-02 12:29:31 +0100

    add exercise.js

---
+++ javascript/javascript1/week3/exercise.js
@@ -0,0 +1,70 @@
+/* Item array removal */
+const names = [
+  "Peter",
+  "Ahmad",
+  "Yana",
+  "kristina",
+  "Rasmus",
+  "Samuel",
+  "katrine",
+  "Tala",
+];
+const nameToRemove = "Ahmad";
+names.splice(names.indexOf(nameToRemove), 1);
+console.log(names);
+
+/* When will we be there?? */
+const travelInformation = {
+  speed: 50,
+  destinationDistance: 432,
+};
+const hour = parseInt(
+  travelInformation.destinationDistance / travelInformation.speed
+);
+const minutes = travelInformation.destinationDistance % travelInformation.speed;
+const travelTime = `${hour} hours and ${minutes} minutes`;
+console.log(travelTime);
+
+/* Series duration of my life */
+const seriesDurations = [
+  {
+    title: "Modern Family",
+    days: 8,
+    hours: 14,
+    minutes: 28,
+  },
+  {
+    title: "Reply 1988",
+    days: 1,
+    hours: 16,
+    minutes: 0,
+  },
+  {
+    title: "American Horror Story",
+    days: 4,
+    hours: 3,
+    minutes: 24,
+  },
+];
+const percentageInLife = [];
+let totalTime = 0;
+function logOutSeriesText() {
+  for (i = 0; i < seriesDurations.length; i++) {
+    let percentage =
+      ((seriesDurations[i].days +
+        seriesDurations[i].hours / 24 +
+        seriesDurations[i].minutes / (24 * 60)) /
+        (80 * 360)) *
+      100;
+    percentageInLife.push(percentage);
+    totalTime += percentageInLife[i];
+    console.log(
+      `${seriesDurations[i].title} took ${percentageInLife[i].toFixed(
+        3
+      )}% of my life`
+    );
+  }
+  console.log(`In total that is ${totalTime.toFixed(2)}% of my life`);
+}
+
+logOutSeriesText();
